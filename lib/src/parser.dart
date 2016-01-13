54d81d9c5c7f025d9143115aa7168ad50f79d69d
Natalie Weizenbaum <nweiz@google.com>
2016-01-12 17:20:33 -0800

    Get rid of all the library tags.
    R=rnystrom@google.com
    
    Review URL: https://codereview.chromium.org//1578413003 .

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library glob.single_component;
-
 import 'package:path/path.dart' as p;
 import 'package:string_scanner/string_scanner.dart';
 
--- lib/src/stream_pool.dart
+++ lib/src/stream_pool.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library glob.stream_pool;
-
 import 'dart:async';
 
 /// A pool of streams whose events are unified and emitted through a central
